18cc920092755f20adb45e69e72494be067c2386
hao134 <hsh319ab@gmail.com>
2023-01-06 13:51:20 +0800

    part1 react_simple_info

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,37 @@
-const App = () =>(
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/hao134">hao134</a>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const name = "Liching"
+  const age = 47
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello name="Shih Hao" age = {47-19} />
+      <Hello name={name} age={age}/>
+      <Footer />
+    </>
+  )
+}
+
+
 
 export default App
