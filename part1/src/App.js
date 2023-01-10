834d6381063e08aaa1f337f7aeb631b582cbeafe
hao134 <hsh319ab@gmail.com>
2023-01-10 21:42:16 +0800

    page re-rendering

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,37 +1,7 @@
-const Hello = ({name, age}) => {
-
-  const bornYear = () => new Date().getFullYear() - age
-
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/hao134">hao134</a>
-    </div>
-  )
-}
-
-const App = () => {
-
-  const name = "Liching"
-  const age = 47
-
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Shih Hao" age = {47-19} />
-      <Hello name={name} age={age}/>
-      <Footer />
-    </>
+    <div>{counter}</div>
   )
 }
 
