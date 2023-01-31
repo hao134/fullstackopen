c7b5f13b6508b35d27f742ced5c59a8eac2e955f
hao134 <hsh319ab@gmail.com>
2023-01-31 21:15:54 +0800

    part1-d-complex state

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,48 +1,25 @@
 import { useState } from "react"
 
-
-const Display = ({counter}) => <div>{counter}</div>
-
-
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
-// or simplifiled
-// const Button = ({onClick, text}) => (
-//   <button onClick={onClick}>
-//     {text}
-//   </button>
-// )
-
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [clicks, setClicks] = useState({
+    left:0, right:0
+  })
 
-  const increaseByOne = () => setCounter(counter+1)
-  const decreaseByOne = () => setCounter(counter-1)
-  const setToZero = () => setCounter(0)
+  const handleLeftClick = () =>
+    //use spread syntax ...
+    setClicks({...clicks,left:clicks.left+1})
+
+  const handleRightClick = () =>
+    setClicks({...clicks,right:clicks.right+1})
 
   return (
     <div>
-      <Display counter={counter}></Display>
-      <Button 
-        onClick={increaseByOne}
-        text = 'plus'
-      />
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />
-      <Button
-        onClick={setToZero}
-        text = 'zero'
-      />
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   )
 }
 
-
-export default App
+export default App;
