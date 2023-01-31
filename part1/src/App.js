440c8e043ea96c9a54bc8da84441a9f36f3b4988
hao134 <hsh319ab@gmail.com>
2023-01-31 20:44:13 +0800

    part1-c Component state, event handlers

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,10 +1,48 @@
-const App = (props) => {
-  const {counter} = props
+import { useState } from "react"
+
+
+const Display = ({counter}) => <div>{counter}</div>
+
+
+const Button = (props) => {
   return (
-    <div>{counter}</div>
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
   )
 }
+// or simplifiled
+// const Button = ({onClick, text}) => (
+//   <button onClick={onClick}>
+//     {text}
+//   </button>
+// )
 
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter+1)
+  const decreaseByOne = () => setCounter(counter-1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter}></Display>
+      <Button 
+        onClick={increaseByOne}
+        text = 'plus'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
+      <Button
+        onClick={setToZero}
+        text = 'zero'
+      />
+    </div>
+  )
+}
 
 
 export default App
