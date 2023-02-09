196cb6b8117266979d6578e582c33fc45726e370
hao134 <hsh319ab@gmail.com>
2023-02-09 16:42:31 +0800

    Axios and promises

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,66 +1,53 @@
 import { useState } from 'react'
-import Filter from './components/Filter'
-import Persons from './components/Persons'
-import PersonForm from './components/PersonForm'
+import Note from './components/Note'
 
 const App = (props) => {
-  const [persons, setPersons] = useState(props.persons)
-  const [newPerson, setNewPerson] = useState({name: "", number: ""})
-  const [filter, setFilter] = useState('')
-  const [personsToShow, setPersonsToShow] = useState(persons);
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
-  const addPerson = (event) => {
+  const addNote = (event) => {
     event.preventDefault()
-    const currentName = persons.filter((person)=>person.name===newPerson.name)
-    if(currentName.length===0){
-      const personObject = {
-        name: newPerson.name,
-        number: newPerson.number,
-        date: new Date().toISOString(),
-        id: newPerson.name
-      };
-      setPersons(persons.concat(personObject))
-      setPersonsToShow(persons.concat(personObject))
-    }else{
-      alert(`${newPerson.name} is already added to phonebook`)
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
     }
-    setNewPerson({name: "", number: ""})
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
-  const handleChange = (event) => {
-    // form's name and value
-    const {name, value} = event.target;
-    // form of newPerson: {name: '', number: ''}
-    // when [name] is name is "a" -> add {name: "a"}
-    // when [name] is number is "1" -> add {number: "1"}
-    setNewPerson({...newPerson, [name]: value});
-    // see how it works
-    console.log(newPerson)
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
   }
 
-  const filterByName = (event) => {
-    const search = event.target.value;
-    setFilter(search);
-    setPersonsToShow(
-      persons.filter((person)=>person.name.toLowerCase().includes(search))
-    )
-  }
+  const notesToShow =  showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Filter value={filter} filterByName={filterByName}/>
-      <h2>add a new</h2>
-      <PersonForm 
-        addPerson={addPerson}
-        newPerson={newPerson}
-        handleChange={handleChange}
-      />
-      <h2>Numbers</h2>
-      
-
-      <Persons personsToShow={personsToShow}/>
-
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">Save</button>
+      </form>
     </div>
   )
 }
--- part2/src/index.js
+++ part2/src/index.js
@@ -1,35 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 
 import App from './App'
 
-const persons = [
-  { 
-    name: 'Arto Hellas',
-    number: '0313-314141',
-    date: new Date().toISOString(),//'2019-05-30T19:20:14.298Z',
-    id: 'Arto Hellas',
-  },
-  { 
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    date: new Date().toISOString(),//'2019-05-30T19:20:14.298Z',
-    id: 'Ada Lovelace',
-  },
-  { 
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    date: new Date().toISOString(),//'2019-05-30T19:20:14.298Z',
-    id: 'Dan Abramov',
-  },
-  { 
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    date: new Date().toISOString(),//'2019-05-30T19:20:14.298Z',
-    id: 'Mary Poppendieck',
-  },
+axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    const notes = response.data
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <App notes={notes}/>
+    )
+  })
 
-]
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App persons={persons}/>
-)
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2)
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <App />
+// )
