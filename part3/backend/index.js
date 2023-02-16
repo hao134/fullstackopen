480ca59c942d0d45b2ee710f4ea40868945d91b3
hao134 <hsh319ab@gmail.com>
2023-02-16 18:19:04 +0800

    3c Connecting the backend to a database

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,32 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+
+const password = process.argv[2]
+
+const url=
+    `mongodb+srv://hao134:${password}@cluster0.arcsdvf.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema)
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -50,7 +76,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
