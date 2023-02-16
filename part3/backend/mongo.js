6b1699a8ca398c9686ce073a8505ba490c3ce427
hao134 <hsh319ab@gmail.com>
2023-02-16 17:19:05 +0800

    Fetching objects from the database

--- part3/backend/mongo.js
+++ part3/backend/mongo.js
@@ -16,16 +16,26 @@ mongoose.connect(url)
 const noteSchema = new mongoose.Schema({
     content: String,
     important: Boolean,
+    date: String,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-    content: 'HTML is Easy2',
-    important: true,
-})
+// const note = new Note({
+//     content: 'Javascript is very good not important',
+//     date: new Date(),
+//     important: false,
+// })
 
-note.save().then(result => {
-    console.log('note saved!')
+// note.save().then(result => {
+//     console.log('note saved!')
+//     mongoose.connection.close()
+// })
+
+// {} return everything match, {important: true} return only important item
+Note.find({important: false}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
     mongoose.connection.close()
 })
