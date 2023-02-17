3c00a528740b17c895d95bad8b27235d8e5d4621
hao134 <hsh319ab@gmail.com>
2023-02-17 16:14:06 +0800

    3c Moving error handling into middleware

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -17,6 +17,16 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError'){
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
 //if we want them to be executed before the route event handlers are called. 
 app.use(requestLogger)
 app.use(express.json())
@@ -50,7 +60,7 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
       if (note) {
@@ -59,10 +69,7 @@ app.get('/api/notes/:id', (request, response) => {
         response.status(404).end()
       }
   })
-  .catch(error => {
-    console.log(error)
-    response.status(400).send({ error: 'malformatted id' })
-  })
+  .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -74,6 +81,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 //we are defining middleware functions that are only called if no route handles the HTTP request.
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
