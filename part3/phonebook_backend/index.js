5f6856ca5965820ab1e1ade9c3712b9656544bf2
hao134 <hsh319ab@gmail.com>
2023-02-17 20:58:37 +0800

    part 3c hw phonebook delete func

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -67,13 +67,13 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-// app.delete('/api/persons/:id', (request, response)=>{
-//     const id = Number(request.params.id)
-//     persons = persons.filter(person => person.id !== id)
-
-//     response.status(204).end()
-// })
-
+app.delete('/api/persons/:id', (request, response)=>{
+    Phonebook.findByIdAndRemove(request.params.id)
+       .then(result => {
+        response.status(204).end()
+       })
+       .catch(error => next(error))
+})
 
 
 
