6ab7d58aa2c13d5190ade7da309a55d9696d1fcc
hao134 <hsh319ab@gmail.com>
2023-02-21 21:53:09 +0800

    4b hw 4.13 to 4.14

--- part4_blogList/tests/blog_api.test.js
+++ part4_blogList/tests/blog_api.test.js
@@ -10,85 +10,118 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 }, 100000)
 
-// step 1
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  }, 100000)
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect (response.body).toHaveLength(helper.initialBlogs.length)
+  }, 100000)
+
+  test('blogs have id properties instead of _id properties', async () => {
+    const response = await api.get('/api/blogs')
+
+    const ids = response.body.map((blog) => blog.id)
+    for (const id of ids) {
+      expect(id).toBeDefined()
+    }
+  }, 100000)
 })
 
-afterAll(async () => {
-  await mongoose.connection.close()
+describe('addition of a new blog', () => {
+  test ('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'google search page',
+      author: 'somebody',
+      url: 'https://www.google.com',
+      likes: 1000,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(n => n.title)
+    expect(titles).toContain(
+      'google search page'
+    )
+  }, 100000)
+
+  test ('if like forget to be added, the likes value set to zero', async () => {
+    const newBlog = {
+      title: 'likes is zero?',
+      author: 'nobody',
+      url: 'https://www.google.com.tw',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toBe(0)
+  }, 100000)
+
+  test ('if title or author are lost, return stauts 400', async () => {
+    const newBlog = {
+      likes: 50
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  }, 100000)
+
 })
 
-// step 2
-test('blogs have id properties instead of _id properties', async () => {
-  const response = await api.get('/api/blogs')
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-  const ids = response.body.map((blog) => blog.id)
-  for (const id of ids) {
-    expect(id).toBeDefined()
-  }
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+
+    const titles = blogsAtEnd.map(r => r.title)
+    expect(titles).not.toContain(blogToDelete.title)
+  })
 })
 
-// step 3
-test ('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'google search page',
-    author: 'somebody',
-    url: 'https://www.google.com',
-    likes: 1000,
-  }
+describe('updating a blog', () => {
+  test('update succeeds with status code 200 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api.put(`/api/blogs/${blogToUpdate.id}`).send({ likes: 1000 }).expect(200)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 
-  const titles = blogsAtEnd.map(n => n.title)
-  expect(titles).toContain(
-    'google search page'
-  )
-}, 100000)
+    const updatedBlog = blogsAtEnd[0]
+    expect(updatedBlog.likes).toBe(1000)
+  })
+})
 
-// step 4
-test ('if like forget to be added, the likes value set to zero', async () => {
-  const newBlog = {
-    title: 'likes is zero?',
-    author: 'nobody',
-    url: 'https://www.google.com.tw',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-  expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toBe(0)
-}, 100000)
-
-// step 5
-test ('if title or author are lost, return stauts 400', async () => {
-  const newBlog = {
-    likes: 50
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-}, 100000)
 
 afterAll(() => {
   mongoose.connection.close()
