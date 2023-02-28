8c55f42b376f8069c54be1c6f8052d37ff65a3df
hao134 <hsh319ab@gmail.com>
2023-02-28 16:33:14 +0800

    5b hw step 10 blogs by likes descend shown

--- part5_bloglist_frontend/src/App.js
+++ part5_bloglist_frontend/src/App.js
@@ -108,12 +108,10 @@ const App = () => {
         <Togglable buttonLabel="new blog" ref={blogFormRef}>
           <BlogForm addBlog={addBlog} />
         </Togglable>
-        {blogs.map(blog =>(
-          <Blog 
-            key={blog.id} 
-            blog={blog} 
-            addLikes={addLikes}
-          />
+        {blogs
+          .sort((a,b) => b.likes - a.likes)
+          .map(blog =>(
+            <Blog key={blog.id} blog={blog} addLikes={addLikes}/>
         ))}
         </div>
       }
