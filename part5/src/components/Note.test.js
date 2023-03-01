30a4a81ac7bdc883e79fd2bc52832416f8c94795
hao134 <hsh319ab@gmail.com>
2023-03-01 12:42:10 +0800

    Debugging tests

--- part5/src/components/Note.test.js
+++ part5/src/components/Note.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Note from './Note'
 
 test('renders content', () => {
@@ -9,10 +9,9 @@ test('renders content', () => {
     important: true
   }
 
-  const { container } = render(<Note note={note} />)
+  render(<Note note={note} />)
 
-  const div = container.querySelector('.note')
-  expect(div).toHaveTextContent(
-    'Component testing is done with react-testing-library'
-  )
+  const element = screen.getByText('Component testing is done with react-testing-library')
+  screen.debug(element)
+  expect(element).toBeDefined()
 })
