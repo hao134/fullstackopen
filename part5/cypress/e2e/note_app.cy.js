3893d1140a570c2b5675081b6573ea8077984af9
hao134 <hsh319ab@gmail.com>
2023-03-02 21:58:14 +0800

    5d Bypassing the UI

--- part5/cypress/e2e/note_app.cy.js
+++ part5/cypress/e2e/note_app.cy.js
@@ -1,23 +1,23 @@
 describe('Note app', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
     const user = {
       name: 'Matti Luukkainen',
       username: 'mluukkai',
       password: 'salainen'
     }
-    cy.request('POST', 'http://localhost:3001/api/users/', user)
-    cy.visit('http://localhost:3000')
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
+    cy.visit('')
   })
 
   it('front page can be opened', function() {
-    cy.visit('http://localhost:3000')
+    cy.visit('')
     cy.contains('Notes')
     cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
   })
 
   it('login form can be opened', function() {
-    cy.visit('http://localhost:3000')
+    cy.visit('')
     cy.contains('log in').click()
   })
 
@@ -46,10 +46,7 @@ describe('Note app', function() {
 
   describe('when logged in', function() {
     beforeEach(function() {
-      cy.contains('log in').click()
-      cy.get('#username').type('mluukkai')
-      cy.get('#password').type('salainen')
-      cy.get('#login-button').click()
+      cy.login({ username: 'mluukkai', password: 'salainen' })
     })
 
     it('a new note can be created', function() {
@@ -61,9 +58,10 @@ describe('Note app', function() {
 
     describe('and a note exists', function () {
       beforeEach(function () {
-        cy.contains('new note').click()
-        cy.get('#noteInput').type('another note cypress')
-        cy.contains('save').click()
+        cy.createNote({
+          content: 'another note cypress',
+          important: true
+        })
       })
 
       it('it can be made important', function() {
