872e86ab58b3335eb0df0a083acdfe9cf1901b49
hao134 <hsh319ab@gmail.com>
2023-03-02 17:38:06 +0800

    5d Writing to a form

--- part5/cypress/e2e/note_app.cy.js
+++ part5/cypress/e2e/note_app.cy.js
@@ -1,7 +1,25 @@
 describe('Note app', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:3000')
+  })
+
   it('front page can be opened', function() {
     cy.visit('http://localhost:3000')
     cy.contains('Notes')
     cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
+  })
+
+  it('login form can be opened', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('log in').click()
+  })
+
+  it('user can login', function() {
+    cy.contains('log in').click()
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('salainen')
+    cy.get("#login-button").click()
+
+    cy.contains('Matti Luukkainen logged in')
   })
 })
