0d079549bf43a8239ff8f209a94aa4635a1825c0
hao134 <hsh319ab@gmail.com>
2023-03-04 22:17:48 +0800

    6a Forwarding Redux Store to various components

--- part6/src/App.js
+++ part6/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { createNote, toggleImportanceOf } from './reducers/noteReducer'
+import { useSelector, useDispatch } from 'react-redux'
 
-function App() {
+
+const App = () => {
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state)
+  const importance = useSelector(state => state.filter(note => note.important))
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    dispatch(createNote(content))
+  }
+
+  const toggleImportance = (id) => {
+    dispatch(toggleImportanceOf(id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      <p>show all notes</p>
+      <ul>
+        {notes.map(note=>
+          <li 
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}
+          >
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>  
+        )}
+      </ul>
+      <p>show important notes</p>
+      <ul>
+        {importance.map(note=>
+          <li
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}
+          >
+            {note.content} <strong>important</strong>
+          </li>  
+          
+        )}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- part6/src/index.js
+++ part6/src/index.js
@@ -1,68 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import {  legacy_createStore as createStore} from 'redux'
+import { Provider } from 'react-redux'
+
+import App from './App'
 import noteReducer from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
 
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
-
-const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    payload: {
-      content,
-      important: false,
-      id: generateId()
-    }
-  }
-}
-
-store.dispatch(createNote('the app state is in redux store'))
-store.dispatch(createNote('state changes are made with actions'))
-
-const toggleImportance = (id) => {
-  store.dispatch({
-    type: 'TOGGLE_IMPORTANCE',
-    payload: { id }
-  })
-}
-
-const App = () => {
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    store.dispatch(createNote(content))
-  }
-
-  return (
-    <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {store.getState().map(note=>
-          <li 
-            key={note.id}
-            onClick={() => toggleImportance(note.id)}
-          >
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>  
-        )}
-      </ul>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const renderApp = () => {
-  root.render(<App />)
-}
-
-// for the purpose of first render
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
