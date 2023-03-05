618fc709ea4b0238a58a23924e5a35cd6436377d
hao134 <hsh319ab@gmail.com>
2023-03-05 14:25:09 +0800

    6a More components

--- part6/src/App.js
+++ part6/src/App.js
@@ -1,52 +1,11 @@
-import { createNote, toggleImportanceOf } from './reducers/noteReducer'
-import { useSelector, useDispatch } from 'react-redux'
-
+import NewNote from "./components/Newnote"
+import Notes from "./components/Notes"
 
 const App = () => {
-  const dispatch = useDispatch()
-  const notes = useSelector(state => state)
-  const importance = useSelector(state => state.filter(note => note.important))
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    dispatch(createNote(content))
-  }
-
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id))
-  }
-
-  return (
+  return(
     <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <p>show all notes</p>
-      <ul>
-        {notes.map(note=>
-          <li 
-            key={note.id}
-            onClick={() => toggleImportance(note.id)}
-          >
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>  
-        )}
-      </ul>
-      <p>show important notes</p>
-      <ul>
-        {importance.map(note=>
-          <li
-            key={note.id}
-            onClick={() => toggleImportance(note.id)}
-          >
-            {note.content} <strong>important</strong>
-          </li>  
-          
-        )}
-      </ul>
+      <NewNote />
+      <Notes />
     </div>
   )
 }
