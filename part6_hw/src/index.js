abf33fc3c374058874bf14edc60d9117f9d742e2
hao134 <hsh319ab@gmail.com>
2023-03-05 20:59:21 +0800

    part 6a hw 6.3 voting func

--- part6_hw/src/index.js
+++ part6_hw/src/index.js
@@ -1,49 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import counterReducer from './reducer'
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import {  legacy_createStore as createStore} from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
 
+const store = createStore(reducer)
 
-const store = createStore(counterReducer)
-
-const App = () => {
-  return (
-    <div>
-      <div>
-        <button
-          onClick={e => store.dispatch({ type: 'GOOD' })}
-        >
-          good
-        </button>
-        <button
-          onClick={e => store.dispatch({ type: 'OK' })}
-        >
-          ok
-        </button>
-        <button
-          onClick={e => store.dispatch({ type: 'BAD' })}
-        >
-          bad
-        </button>
-        <button
-          onClick={e => store.dispatch({ type: 'ZERO' })}
-        >
-          reset
-        </button>
-        <div>good {store.getState().good}</div>
-        <div>ok {store.getState().ok}</div>
-        <div>bad {store.getState().bad}</div>
-      </div>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const renderApp = () => {
-  root.render(<App />)
-}
-
-// for the purpose of first render
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
