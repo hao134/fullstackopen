5df2e755170f9c417c4a5c8f2d600cc46a6b3bbc
hao134 <hsh319ab@gmail.com>
2023-03-06 13:39:56 +0800

    part 6a hw 6.5 sort the anecdotes by votes

--- part6_hw/src/App.js
+++ part6_hw/src/App.js
@@ -2,45 +2,43 @@ import { useSelector, useDispatch } from 'react-redux'
 import { addVote, createAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+    const anecdotes = useSelector(state => state)
+    const dispatch = useDispatch()
 
-  const handleAddVote = (id) => {
-    dispatch(addVote(id))
-  }
+    const handleAddVote = (id) => {
+        dispatch(addVote(id))
+    }
 
-  const handleAddAnecdote = (event) =>{
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-  }
+    const handleAddAnecdote = (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        event.target.anecdote.value = ''
+        dispatch(createAnecdote(content))
+    }
 
-  return (
-    <div>
-      <form onSubmit={handleAddAnecdote}>
-        <input name="anecdote"/>
-        <button type="submit">add</button>
-      </form>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}{' '}
-            <button onClick={() => handleAddVote(anecdote.id)}>vote</button>
-          </div>
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            {anecdotes
+                .sort((a, b) => b.votes - a.votes)
+                .map(anecdote =>
+                    <div key={anecdote.id}>
+                        <div>
+                            {anecdote.content}
+                        </div>
+                        <div>
+                            has {anecdote.votes}{' '}
+                            <button onClick={() => handleAddVote(anecdote.id)}>vote</button>
+                        </div>
+                    </div>
+                )}
+            <h2>create new</h2>
+            <form onSubmit={handleAddAnecdote}>
+                <input name="anecdote" />
+                <button type="submit">add</button>
+            </form>
         </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
+    )
 }
 
 export default App
