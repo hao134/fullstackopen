d2f6b58b03b9a3a59c9c91f7b8ced25ed1c7b1be
hao134 <hsh319ab@gmail.com>
2023-03-06 17:07:05 +0800

    6b Store with complex state

--- part6/src/App.js
+++ part6/src/App.js
@@ -2,9 +2,20 @@ import NewNote from "./components/Newnote"
 import Notes from "./components/Notes"
 
 const App = () => {
-  return(
+  const filterSelected = (value) => {
+    console.log(value)
+  }
+  return (
     <div>
       <NewNote />
+      <div>
+        all          <input type="radio" name="filter"
+          onChange={() => filterSelected('ALL')} />
+        important    <input type="radio" name="filter"
+          onChange={() => filterSelected('IMPORTANT')} />
+        nonimportant <input type="radio" name="filter"
+          onChange={() => filterSelected('NONIMPORTANT')} />
+      </div>
       <Notes />
     </div>
   )
