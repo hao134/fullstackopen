9d50194bc108ad421a6955d4ecfc07e4c865c54c
hao134 <hsh319ab@gmail.com>
2023-03-06 22:05:03 +0800

    6b finishing the filtering

--- part6/src/index.js
+++ part6/src/index.js
@@ -7,9 +7,6 @@ import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
-
 const reducer = combineReducers({
   notes: noteReducer,
   filter: filterReducer
@@ -17,22 +14,8 @@ const reducer = combineReducers({
 
 const store = createStore(reducer)
 
-
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
-
-
-/*
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-  </Provider>
-)*/
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <div />
   </Provider>
 )
