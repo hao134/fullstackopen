dfdb10ffa22e3c701cf6b464b1c63a178a7f17c0
hao134 <hsh319ab@gmail.com>
2023-03-08 11:46:22 +0800

    6c getting data from the backend

--- part6/src/index.js
+++ part6/src/index.js
@@ -4,8 +4,9 @@ import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
 
-import noteReducer from './reducers/noteReducer'
+import noteReducer,  { setNotes } from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
+import noteService from './services/notes'
 
 
 const store = configureStore({
@@ -14,6 +15,10 @@ const store = configureStore({
     filter: filterReducer
   }
 })
+
+noteService.getAll().then(notes =>
+  store.dispatch(setNotes(notes))
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
