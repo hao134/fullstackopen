45e77f3bfc9b8258bf8fcc607ea0947c0ea8e506
hao134 <hsh319ab@gmail.com>
2023-03-13 15:45:37 +0800

    6d Defining the counter context in a separate file

--- part6_3_hook-counter/src/App.js
+++ part6_3_hook-counter/src/App.js
@@ -1,45 +1,17 @@
-import { useReducer } from 'react'
 import Button from './components/Button'
 import Display from './components/Display'
-import CounterContext from './CounterContext'
-
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case "INC":
-        return state + 1
-    case "DEC":
-        return state - 1
-    case "ZERO":
-        return 0
-    default:
-        return state
-  }
-}
-
 
 const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer,0)
-
   return (
-    <CounterContext.Provider value={[counter, counterDispatch]}>
-      <Display counter={counter}/>
+    <div>
+      <Display />
       <div>
         <Button type='INC' label='+' />
         <Button type='DEC' label='-' />
         <Button type='ZERO' label='0' />
       </div>
-    </CounterContext.Provider>
+    </div>
   )
-  // return (
-  //   <CounterContext.Provider value={[counter, counterDispatch]}>
-  //     <div>{counter}</div>
-  //     <div>
-  //       <button onClick={() => counterDispatch({ type: "INC" })}>+</button>
-  //       <button onClick={() => counterDispatch({ type: "DEC" })}>-</button>
-  //       <button onClick={() => counterDispatch({ type: "ZERO" })}>0</button>
-  //     </div>
-  //   </CounterContext.Provider>
-  // )
 }
 
 export default App
