3ca6d739b95c7a4328f715bdbbdf39790cdd52fd
hao134 <hsh319ab@gmail.com>
2023-03-13 12:42:11 +0800

    6d Using context for passing the state to components

--- part6_3_hook-counter/src/App.js
+++ part6_3_hook-counter/src/App.js
@@ -1,4 +1,7 @@
 import { useReducer } from 'react'
+import Button from './components/Button'
+import Display from './components/Display'
+import CounterContext from './CounterContext'
 
 const counterReducer = (state, action) => {
   switch (action.type) {
@@ -13,19 +16,30 @@ const counterReducer = (state, action) => {
   }
 }
 
+
 const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0)
+  const [counter, counterDispatch] = useReducer(counterReducer,0)
 
   return (
-    <div>
-      <div>{counter}</div>
+    <CounterContext.Provider value={[counter, counterDispatch]}>
+      <Display counter={counter}/>
       <div>
-        <button onClick={() => counterDispatch({ type: "INC"})}>+</button>
-        <button onClick={() => counterDispatch({ type: "DEC"})}>-</button>
-        <button onClick={() => counterDispatch({ type: "ZERO"})}>0</button>
+        <Button type='INC' label='+' />
+        <Button type='DEC' label='-' />
+        <Button type='ZERO' label='0' />
       </div>
-    </div>
+    </CounterContext.Provider>
   )
+  // return (
+  //   <CounterContext.Provider value={[counter, counterDispatch]}>
+  //     <div>{counter}</div>
+  //     <div>
+  //       <button onClick={() => counterDispatch({ type: "INC" })}>+</button>
+  //       <button onClick={() => counterDispatch({ type: "DEC" })}>-</button>
+  //       <button onClick={() => counterDispatch({ type: "ZERO" })}>0</button>
+  //     </div>
+  //   </CounterContext.Provider>
+  // )
 }
 
 export default App
