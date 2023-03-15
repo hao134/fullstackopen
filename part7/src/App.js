7d5cacef0f0c3012eaef7267afa094aebb8ce5f8
hao134 <hsh319ab@gmail.com>
2023-03-15 15:18:55 +0800

    rearange

--- part7/src/App.js
+++ part7/src/App.js
@@ -1,4 +1,18 @@
-import useField from './components/useField'
+import { useState } from 'react'
+const useField = (type) => {
+    const [value, setValue] = useState('')
+  
+    const onChange = (event) => {
+      setValue(event.target.value)
+    }
+  
+    return{
+      type,
+      value,
+      onChange
+    }
+}
+
 const App = () => {
   const name = useField('text')
   const born = useField('date')
