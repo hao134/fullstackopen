abdf9b14d634e1525a0be6930396d8b2b3cb8c9d
hao134 <hsh319ab@gmail.com>
2023-03-16 14:40:55 +0800

    hw part 7.8

--- part7_hw/src_ultimate-hooks/App.js
+++ part7_hw/src_ultimate-hooks/App.js
@@ -18,13 +18,18 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    setResources(response.data)
+  }
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources([...resources, response.data])
   }
 
   const service = {
+    getAll,
     create
   }
 
@@ -40,6 +45,12 @@ const App = () => {
 
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
+
+  useEffect(() => {
+    noteService.getAll()
+    personService.getAll()
+    // eslint-disable-next-line
+  },[])
 
   const handleNoteSubmit = (event) => {
     event.preventDefault()
