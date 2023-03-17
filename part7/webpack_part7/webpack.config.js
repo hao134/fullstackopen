a65aba46e8d381531976317e0eecddf520e70b6c
hao134 <hsh319ab@gmail.com>
2023-03-17 18:50:39 +0800

    7d Webpack-dev-server

--- part7/webpack_part7/webpack.config.js
+++ part7/webpack_part7/webpack.config.js
@@ -7,14 +7,23 @@ const config = () => {
       path: path.resolve(__dirname, 'build'),
       filename: 'main.js'
     },
+    devServer: {
+      static: path.resolve(__dirname, 'build'),
+      compress: true,
+      port: 3000,
+    },
     module: {
       rules: [
         {
           test: /\.js$/,
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-react'],
+            presets: ['@babel/preset-env','@babel/preset-react'],
           },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
         },
       ],
     }
