c02f1483740a3bc36e09cf206325a1f5cbd193eb
hao134 <hsh319ab@gmail.com>
2023-03-24 22:00:36 +0800

    8a Enum

--- part8/index.js
+++ part8/index.js
@@ -37,10 +37,14 @@ const typeDefs = `
     address: Address!
     id: ID!
   }
+  enum YesNo{
+    YES
+    NO
+  }
 
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
 
@@ -56,7 +60,14 @@ const typeDefs = `
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone){
+        return persons 
+      }
+      const byPhone = (person) =>
+        args.phone === 'YES' ? person.phone : !person.phone
+      return persons.filter(byPhone)
+    },
     findPerson: (root, args) =>
       persons.find(p => p.name === args.name)
   },
