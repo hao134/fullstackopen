4080437a6a5565d01d1c7456a5f92534737b6145
hao134 <hsh319ab@gmail.com>
2023-03-25 21:45:19 +0800

    hw part8.6 Adding a book

--- part8_hw/index.js
+++ part8_hw/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -117,6 +118,14 @@ const typeDefs = `
     allAuthors: [Author!]!
     allBooks(author: String, genre: String): [Book!]!
   }
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -147,7 +156,23 @@ const resolvers = {
   Author: {
     bookCount: (root) =>
       books.filter((book) => book.author === root.name).length
-  }
+  },
+  Mutation:{
+    addBook: (root, args) => {
+      const author = authors.find((author) => author.name === args.author)
+      if (!author){
+        const newAuthor = {
+          id: uuid(),
+          name: args.author
+        }
+        authors = authors.concat(newAuthor)
+      }
+
+      const book = {...args, id: uuid() };
+      books = books.concat(book)
+      return book
+    }
+  },
 }
 
 const server = new ApolloServer({
