98e4fe461e32a59998dc5118848520315f487816
hao134 <hsh319ab@gmail.com>
2023-03-28 21:36:21 +0800

    hw 8.9: Books view

--- part8_hw/client/src/components/Books.js
+++ part8_hw/client/src/components/Books.js
@@ -1,9 +1,18 @@
+import { useQuery } from '@apollo/client'
+
+import { ALL_BOOKS } from '../queries'
+
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+  if (result.loading){
+    return <div>loading...</div>
+  }
+
   if (!props.show) {
     return null
   }
 
-  const books = []
+  const books = result.data.allBooks || []
 
   return (
     <div>
@@ -16,11 +25,11 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
+          {books.map((b) => (
+            <tr key={b.title}>
+              <td>{b.title}</td>
+              <td>{b.author}</td>
+              <td>{b.published}</td>
             </tr>
           ))}
         </tbody>
