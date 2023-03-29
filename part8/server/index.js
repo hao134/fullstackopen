321742f2a18c31423bdcfbbb50a5c157c7564cf3
hao134 <hsh319ab@gmail.com>
2023-03-29 20:13:29 +0800

    8c Validation

--- part8/server/index.js
+++ part8/server/index.js
@@ -1,6 +1,5 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
-const { v1: uuid } = require('uuid')
 const { GraphQLError } = require('graphql')
 
 const mongoose = require('mongoose')
@@ -100,12 +99,36 @@ const resolvers = {
   Mutation: {
     addPerson : async (root, args) => {
       const person = new Person({ ...args })
-      return person.save()
+      try {
+        await person.save()
+      } catch (error) {
+        throw new GraphQLError('Saving person failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error
+          }
+        })
+      }
+
+      return person
     },
-    editNumber: (root, args) => {
-      const person = persons.findOne({ name: args.name })
+    editNumber: async (root, args) => {
+      const person = await Person.findOne({ name: args.name })
       person.phone = args.phone
-      return person.save()
+      
+      try {
+        await person.save()
+      } catch (error) {
+        throw new GraphQLError('Saving number failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error
+          }
+        })
+      }
+      return person
     }
   }
 }
