7cc719e1c2f7b6b7ee78bacacb59349150367cff
hao134 <hsh319ab@gmail.com>
2023-04-01 15:20:14 +0800

    hw 8.17 fix allbook show

--- part8_hw/client/src/components/Books.js
+++ part8_hw/client/src/components/Books.js
@@ -28,7 +28,7 @@ const Books = (props) => {
           {books.map((b) => (
             <tr key={b.id}>
               <td>{b.title}</td>
-              <td>{b.author}</td>
+              <td>{b.author.name}</td>
               <td>{b.published}</td>
             </tr>
           ))}
