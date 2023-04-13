3bc1c782840977e576925f74e03eac72372b97c1
hao134 <hsh319ab@gmail.com>
2023-04-13 18:08:36 +0800

    9c Adding a new diary

---
+++ part9/part9c/readme.md
@@ -0,0 +1,2 @@
+## part9c adding a new diary -> result:
+![](https://i.imgur.com/me8OvTk.png)
--- part9/part9c/src/services/diaryService.ts
+++ part9/part9c/src/services/diaryService.ts
@@ -1,6 +1,10 @@
 import diaries from '../../data/entries';
 
-import { NonSensitiveDiaryEntry, DiaryEntry } from '../types';
+import { 
+  NonSensitiveDiaryEntry, 
+  DiaryEntry,
+  NewDiaryEntry
+} from '../types';
 
 const getEntries = (): DiaryEntry[] => {
     return diaries;
@@ -15,8 +19,14 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
     }));
 };
 
-const addDiary = () => {
-  return null;
+const addDiary = ( entry: NewDiaryEntry ): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
 };
 
 const findById = (id: number): DiaryEntry | undefined => {
