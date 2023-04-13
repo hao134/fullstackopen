4b00032aa11a59b50e892af7cbf6113d48b71a8b
hao134 <hsh319ab@gmail.com>
2023-04-13 12:30:09 +0800

    hw 9.10 Patientor backend, step3

---
+++ part9_hw/part9c_patientor/part9c_backend/src/services/diagnoseService.ts
@@ -0,0 +1,11 @@
+import diagnoseData from '../../data/diagnoses';
+
+import { DiagnoseEntry } from '../types';
+
+const getEntries = (): DiagnoseEntry[] => {
+  return diagnoseData;
+};
+
+export default {
+  getEntries
+};
