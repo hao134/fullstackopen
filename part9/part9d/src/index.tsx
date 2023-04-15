6fa060b9fbf2f9ce4a21c7a6db72c18b8679a110
hao134 <hsh319ab@gmail.com>
2023-04-15 18:32:55 +0800

    9d Deeper type usage

--- part9/part9d/src/index.tsx
+++ part9/part9d/src/index.tsx
@@ -1,21 +1,6 @@
 import ReactDOM from 'react-dom/client'
-
-interface WelcomeProps {
-  name: string;
-}
-
-// const Welcome = (props: WelcomeProps): JSX.Element => {
-//   return <h1>Hello, {props.name}</h1>
-// };
-
-// const Welcome = ({ name }: { name: string }): JSX.Element => (
-//   <h1>Hello, {name}</h1>
-// );
-
-const Welcome = (props: WelcomeProps) => {
-  return <h1>Hello, {props.name}</h1>
-};
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Welcome name="Sarah" />
+  <App />
 )
