48e37152b23db350cde2c7b0fcb1783c28a9f234
hao134 <hsh319ab@gmail.com>
2023-04-19 18:54:35 +0800

    9e Full entries

--- part9_hw/part9c_patientor/part9c_backend/src/types.ts
+++ part9_hw/part9c_patientor/part9c_backend/src/types.ts
@@ -10,8 +10,50 @@ export interface DiagnoseEntry {
     latin?: string;
 }
 
-export interface Entry {
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<DiagnoseEntry['code']>;
 }
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+export interface SickLeave {
+    startDate: string,
+    endDate: string
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    sickLeave?: SickLeave;
+    employerName: string;
+}
+
+export interface HospitalDischarge {
+    date: string;
+    criteria: string;
+}
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: HospitalDischarge;
+}
+
+export type Entry = 
+ | HealthCheckEntry
+ | OccupationalHealthcareEntry
+ | HospitalEntry;
 
 export interface Patient {
     id: string;
